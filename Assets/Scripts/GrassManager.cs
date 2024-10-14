adca773f1fddf722a556acf24e20656cfe1c0e95
iFrexsim <47043939+Frexsim@users.noreply.github.com>
2024-10-14 16:22:42 +0200

    Finish grass and add camera panning

--- Assets/Scripts/GrassManager.cs
+++ Assets/Scripts/GrassManager.cs
@@ -1,18 +1,61 @@
 using UnityEngine;
-using UnityEngine.Tilemaps;
+using UnityEngine.InputSystem;
 
 public class GrassManager : MonoBehaviour
 {
-    [SerializeField] Tilemap tilemap;
+    [SerializeField] Vector2Int lawnSize = new Vector2Int(10, 10);
+    [SerializeField] float panSensitivity = 0.1f;
 
+    [SerializeField] GrassTileBase grassTilePrefab;
 
+    GrassTileBase[,] grassTiles;
 
-    void OnCutGrass()
+    bool isPanning = false;
+
+    private void Awake()
     {
-        Vector3Int tilemapPos = tilemap.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        grassTiles = new GrassTileBase[lawnSize.x, lawnSize.y];
+    }
 
-        Tile tile = tilemap.GetTile<Tile>(tilemapPos);
+    private void Start()
+    {
+        GenerateTiles();
+    }
 
-        Debug.Log(tilemapPos);
+    private void GenerateTiles()
+    {
+        for (int x = 0; x < lawnSize.x; x++)
+        {
+            for (int y = 0; y < lawnSize.y; y++)
+            {
+                grassTiles[x, y] = Instantiate(grassTilePrefab, new Vector3(x, y, 0), Quaternion.identity);
+            }
+        }
+    }
+
+    public void OnCutGrass()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, 100f))
+        {
+            GrassTileBase grassTile = raycastHit.transform.GetComponent<GrassTileBase>();
+            if (grassTile != null)
+            {
+                grassTile.Cut();
+            }
+        }
+    }
+
+    public void OnPan()
+    {
+        isPanning = InputSystem.actions.FindAction("Pan").WasPressedThisFrame();
+    }
+
+    public void OnPanMove(InputValue value)
+    {
+        if (isPanning)
+        {
+            Camera.main.transform.position += Camera.main.ScreenToViewportPoint((Vector3) value.Get<Vector2>()) * panSensitivity;
+        }
     }
 }
--- Assets/Scripts/GrassTiles/GrassTileBase.cs
+++ Assets/Scripts/GrassTiles/GrassTileBase.cs
@@ -1,22 +1,57 @@
-using System;
-using Unity.VisualScripting;
+using System.Collections;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
-[Serializable]
-public class GrassTileBase : TileBase
+public class GrassTileBase : MonoBehaviour
 {
-    [SerializeField] Sprite[] sprites;
+    [SerializeField] Sprite[] grassTileLevelSprites;
+    [SerializeField, Min(0.5f)] float growthSpeedMin;
+    [SerializeField, Min(0.5f)] float growthSpeedMax;
 
-    private void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
+    [SerializeField] int maxHealth;
+    [SerializeField] int health;
+
+    SpriteRenderer spriteRenderer;
+
+    private void Awake()
     {
-        tileData.sprite = sprites[0];
+        health = 0;
 
-        tileData.transform = this.transform;
-        tileData.color = this.color;
-        tileData.gameObject = this.gameObject;
-        tileData.flags = this.flags;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        UpdateSprite();
+    }
 
-        tileData.colliderType = this.colliderType;
+    private void Start()
+    {
+        StartCoroutine("GrowLoop");
+    }
+
+    public void Cut()
+    {
+        if (health > 0)
+        {
+            health -= 1;
+            UpdateSprite();
+        }
+    }
+
+    private void UpdateSprite()
+    {
+        int spriteIndex = Mathf.FloorToInt((float) health / (maxHealth / (grassTileLevelSprites.Length - 1)));
+
+        Sprite sprite = grassTileLevelSprites[spriteIndex];
+        spriteRenderer.sprite = sprite;
+    }
+
+    private IEnumerator GrowLoop()
+    {
+        yield return new WaitForSeconds(Random.Range(growthSpeedMin, growthSpeedMax));
+
+        if (health + 1 <= maxHealth)
+        {
+            health += 1;
+            UpdateSprite();
+        }
+
+        StartCoroutine("GrowLoop");
     }
 }
